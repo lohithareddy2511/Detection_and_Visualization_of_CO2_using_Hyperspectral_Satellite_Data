2d727c1d6d3f3e086893b3711aab609be34ff0f0
meenakshy2004 <159451092+meenakshy2004@users.noreply.github.com>
2024-12-06 22:23:58 +0530

    Final_Visualization.m

--- Code/Final_Visualization.m
+++ Code/Final_Visualization.m
@@ -6,3 +6,8 @@ co2Anomalies = co2DetectionMap > threshold;
 figure;
 imagesc(co2DetectionMap);
 title('CO2 Detection Map');
+
+%Final Outputs
+![Screenshot 2024-12-06 200814](https://github.com/user-attachments/assets/d95981b2-7a69-4cfb-a13a-3f3a48b59486)
+![Screenshot 2024-12-06 200756](https://github.com/user-attachments/assets/7ce22865-2f38-40ed-a8c2-1c29671f9427)
+![Screenshot 2024-12-06 200743](https://github.com/user-attachments/assets/ea77ce1c-d397-4ccc-aa21-21eba490b5ee)
