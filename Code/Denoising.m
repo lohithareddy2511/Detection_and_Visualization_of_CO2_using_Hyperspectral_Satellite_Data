f126143e511039a30fefc16aa3de75646993cb73
meenakshy2004 <159451092+meenakshy2004@users.noreply.github.com>
2024-12-06 22:15:55 +0530

    Denoising.m

--- Code/Denoising.m
+++ Code/Denoising.m
@@ -4,6 +4,3 @@ smoothedData = imgaussfilt3(dataCube, sigma);
 figure;
 imagesc(smoothedData(:, :, 4));  % Display one of the bands, for example, Band 4
 title('Denoised Image after Gaussian Smoothing');
-
-
-
