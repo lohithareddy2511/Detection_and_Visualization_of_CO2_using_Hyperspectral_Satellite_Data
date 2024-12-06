e172727a830cb39ba5a65e9958f9520bba47712d
meenakshy2004 <159451092+meenakshy2004@users.noreply.github.com>
2024-12-06 21:56:28 +0530

    Clustering.m

---
+++ K_Means_Clustering.m
@@ -0,0 +1,18 @@
+% Reshape data cube for clustering
+[numRows, numCols, numBands] = size(smoothedData);
+reshapedData = reshape(smoothedData, [], numBands);
+
+% Perform K-Means clustering
+numClusters = 5; 
+[clusterIdx, ~] = kmeans(reshapedData, numClusters, 'MaxIter', 1000);
+
+% Reshape clustering results back to image dimensions
+clusteredImage = reshape(clusterIdx, numRows, numCols);
+%clusteredImage = (clusteredImage - min(clusteredImage(:))) / (max(clusteredImage(:)) - min(clusteredImage(:)));
+
+
+% Visualize clustering result
+figure;
+imagesc(clusteredImage);
+title('Normalized K-Means Clustering Result');
+
