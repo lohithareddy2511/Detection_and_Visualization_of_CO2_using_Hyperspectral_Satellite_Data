b280890be62898a85e29820a4de7eafeec4f2484
Karamala Lohitha Reddy <lohithareddy2511@gmail.com>
2024-12-11 10:10:52 +0530

    Update Mapping_on_Worldmap.m

--- Code/Mapping_on_Worldmap.m
+++ Code/Mapping_on_Worldmap.m
@@ -1,1 +1,19 @@
+minLat = -30; 
+maxLat = 30;  
+minLon = 70;  
+maxLon = 130; 
 
+
+% Create geographic coordinates
+lat = linspace(minLat, maxLat, numRows);
+lon = linspace(minLon, maxLon, numCols);
+[LON, LAT] = meshgrid(lon, lat);
+
+% Visualize CO2 detection map
+figure;
+worldmap([minLat, maxLat], [minLon, maxLon]);
+load coastlines;
+plotm(coastlat, coastlon, 'k'); % Plot coastlines
+geoshow(LAT, LON, co2DetectionMap, 'DisplayType', 'texturemap');
+colorbar;
+title('CO2 Detection Map on World Map');
