5bf6a253b77303935816b243df87b25d4825dfaf
Karamala Lohitha Reddy <lohithareddy2511@gmail.com>
2024-12-15 14:59:37 +0530

    Delete Code directory

--- Code/Denoising.m
+++
@@ -1,6 +0,0 @@
-sigma = 1;
-smoothedData = imgaussfilt3(dataCube, sigma);
-
-figure;
-imagesc(smoothedData(:, :, 4));  % Display one of the bands, for example, Band 4
-title('Denoised Image after Gaussian Smoothing');
