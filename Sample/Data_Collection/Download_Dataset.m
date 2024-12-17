01b2770ecc467f8a5ed4f7855e31b330c8c0fe2d
Karamala Lohitha Reddy <lohithareddy2511@gmail.com>
2024-12-17 10:29:16 +0530

    Delete Sample/Data_Collection directory

--- Sample/Data_Collection/Download_Dataset.m
+++
@@ -1,5 +0,0 @@
-%Use this code to download the dataset into MATLAB Online Compiler and then import it into the local device 
-
-zipfile = "LC08_L1TP_113082_20211206_20211206_01_RT.zip";
-landsat8Data_url = "https://ssd.mathworks.com/supportfiles/image/data/" + zipfile;
-downloadLandsat8Dataset(landsat8Data_url,pwd)
